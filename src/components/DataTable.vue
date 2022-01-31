4676dbea472f82ca59a0ec7c9ce666e80c577a7e
muneebhashone <muneeb.hashone@gmail.com>
2022-01-31 13:34:59 +0500

    Working on data table

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,36 +1,58 @@
 <template>
-  <v-data-table :headers="headers" :items="desserts" :items-per-page="5">
-    <template v-slot:item="row">
-      <tr>
-        <td>{{ row.item.title }}</td>
-        <td>{{ row.item.author }}</td>
-        <td>{{ row.item.status }}</td>
-        <td>{{ row.item.published }}</td>
-        <td>{{ row.item["last-edited"] }}</td>
-        <td>
-          <div class="data-actions">
-            <CustomButton
-              @onClick="onEditClick(row.item)"
-              :noBorder="true"
-              :invert="true"
-              text="Edit"
-            />
-            <CustomButton
-              @onClick="onDuplicateClick(row.item)"
-              :noBorder="true"
-              :invert="true"
-              text="Duplicate"
-            />
-            <CustomButton
-              @onClick="onDeleteClick(row.item)"
-              :noBorder="true"
-              :invert="true"
-              text="Delete"
-            />
-          </div>
-        </td>
-      </tr> </template
-  ></v-data-table>
+  <div class="data-table-blog-list">
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="itemsPerPage"
+      :page.sync="page"
+      @page-count="pageCount = $event"
+      hide-default-footer
+    >
+      <template v-slot:item="row">
+        <tr>
+          <td>{{ row.item.title }}</td>
+          <td>{{ row.item.author }}</td>
+          <td>{{ row.item.status }}</td>
+          <td>{{ row.item.published }}</td>
+          <td>{{ row.item["last-edited"] }}</td>
+          <td>
+            <div class="data-actions">
+              <CustomButton
+                @onClick="onEditClick(row.item)"
+                :noBorder="true"
+                :invert="true"
+                text="Edit"
+              />
+              <CustomButton
+                @onClick="onDuplicateClick(row.item)"
+                :noBorder="true"
+                :invert="true"
+                text="Duplicate"
+              />
+              <CustomButton
+                @onClick="onDeleteClick(row.item)"
+                :noBorder="true"
+                :invert="true"
+                text="Delete"
+              />
+            </div>
+          </td>
+        </tr> </template
+    ></v-data-table>
+    <v-pagination
+      v-model="page"
+      :total-visible="9"
+      :length="pageCount"
+    ></v-pagination>
+    <v-text-field
+      :value="itemsPerPage"
+      label="Items per page"
+      type="number"
+      min="-1"
+      max="15"
+      @input="itemsPerPage = parseInt($event, 10)"
+    ></v-text-field>
+  </div>
 </template>
 
 <script>
@@ -39,6 +61,9 @@ export default {
   components: { CustomButton },
   data() {
     return {
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
       headers: [
         {
           text: "Title",
@@ -55,13 +80,17 @@ export default {
           value: "actions",
         },
       ],
-      desserts: Array(10).fill({
-        title: "Title",
-        author: "Admin",
-        status: "Draft",
-        published: "2021/12/04 20:30:33",
-        "last-edited": "2021/12/04 20:30:33",
-      }),
+      desserts: Array(20)
+        .fill({
+          title: "Title",
+          author: "Admin",
+          status: "Draft",
+          published: "2021/12/04 20:30:33",
+          "last-edited": "2021/12/04 20:30:33",
+        })
+        .map((item, index) => {
+          return { ...item, title: item.title + index };
+        }),
     };
   },
   methods: {
@@ -83,15 +112,47 @@ export default {
   display: flex;
 }
 
+.data-table-blog-list {
+  & tbody > tr td:nth-child(1),
+  & thead > tr th:nth-child(1) {
+    width: 324px !important;
+  }
+
+  & thead > tr th span {
+    color: #272e3b;
+  }
+}
+
+button.v-pagination__item.v-pagination__item--active.primary {
+  background: #e8fffd;
+  border-radius: 4px;
+  color: #217d8d;
+}
+
+button.v-pagination__item {
+  box-shadow: none;
+  border-radius: 4px;
+}
+
+button.v-pagination__navigation.v-pagination__navigation {
+  box-shadow: none;
+  border-radius: 4px;
+}
+
+.v-pagination__more {
+  letter-spacing: 7px;
+  align-items: center;
+  margin: 0.5rem;
+  top: -3px;
+  position: relative;
+}
+
 .v-data-table-header {
   background-color: #f7f8fa;
 
-  & tbody > tr > td:nth-child(1) {
-    width: 30%;
-  }
-
   & tr th {
     text-align: left;
+
     & span {
       font-weight: 500;
       font-size: 14px;
